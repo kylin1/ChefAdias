9e965106581f6a6ae9118bcd433e03feb67611b2
xzf14 <141250164@smail.nju.edu.cn>
2016-12-07 11:28:21 +0800

    更新ticket

--- src/main/java/web/biz/impl/TicketImpl.java
+++ src/main/java/web/biz/impl/TicketImpl.java
@@ -6,10 +6,13 @@ import web.biz.TicketService;
 import web.dao.OrderDao;
 import web.dao.TicketDao;
 import web.dao.UserTicketDao;
+import web.model.po.Order;
 import web.model.po.UserTicket;
 import web.model.vo.TickInfoVO;
 import web.tools.MyMessage;
 
+import java.math.BigDecimal;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -22,26 +25,30 @@ public class TicketImpl implements TicketService {
     private TicketDao ticketDao;
     @Autowired
     private OrderDao orderDao;
-
     @Autowired
     private UserTicketDao userTicketDao;
 
-
     @Override
     public TickInfoVO getTicketInfo(int userId) {
+        //UserTicketDAO
         List<UserTicket> ticketList = userTicketDao.getUserTicket(userId);
         //list size为1
         UserTicket ticket = ticketList.get(0);
-//        BigDecimal dailyUpper = ticket.getDaily_upper();
-//        Date expireTime = ticket.getExpire_time();
-//
-//        List<Order> orderList = orderDao.getOrderOfUser(userId);
-//        for(Order order:orderList){
-//            Date createTime = order.getCreate_time();
-//            createTime.compareTo(expireTime);
-//        }
+        Date expireTime = ticket.getExpire_time();
+        int tickID = ticket.getTicket_id();
+
+        //TicketDAO
 
 
+        //OrderDAO
+        List<Order> orderList = orderDao.getOrderOfUser(userId);
+        BigDecimal dailySum = new BigDecimal(0);
+        for (Order order : orderList) {
+            Date createTime = order.getCreate_time();
+            if (createTime.compareTo(expireTime) == 0) {
+                dailySum = dailySum.add(order.getPrice());
+            }
+        }
         return null;
     }
 
