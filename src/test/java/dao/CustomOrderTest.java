a99a81e48d454d6f2c45cbf0136fe79f5b122279
kylin1 <18795853969@163.com>
2016-12-17 16:38:30 +0800

    remove test

--- src/test/java/dao/CustomOrderTest.java
+++ src/test/java/dao/CustomOrderTest.java
@@ -1,6 +1,5 @@
 package dao;
 
-import org.junit.Test;
 import web.dao.impl.CustomOrderDaoImpl;
 import web.model.po.CustomOrder;
 import web.tools.CheckClass;
@@ -14,7 +13,7 @@ import java.util.List;
  */
 public class CustomOrderTest {
 
-    @Test
+//    @Test
     public void getOrderInDay() throws ClassNotFoundException {
         CustomOrderDaoImpl dao = new CustomOrderDaoImpl();
         List<CustomOrder> list = dao.getOrderInDay(1,"2016-12-17", "2016-12-18");
@@ -23,7 +22,7 @@ public class CustomOrderTest {
         }
     }
 
-    @Test
+//    @Test
     public void getOrderOfUser() throws ClassNotFoundException {
         CustomOrderDaoImpl dao = new CustomOrderDaoImpl();
         List<CustomOrder> list = dao.getOrderOfMenu(1);
@@ -32,7 +31,7 @@ public class CustomOrderTest {
         }
     }
 
-    @Test
+//    @Test
     public void getOrder() throws ClassNotFoundException {
         CustomOrderDaoImpl dao = new CustomOrderDaoImpl();
         CustomOrder order = dao.getOrder(1);
@@ -40,7 +39,7 @@ public class CustomOrderTest {
     }
 
 
-    @Test
+//    @Test
     public void add() throws ClassNotFoundException {
         CustomOrderDaoImpl dao = new CustomOrderDaoImpl();
         CustomOrder order = new CustomOrder();
@@ -57,7 +56,7 @@ public class CustomOrderTest {
         System.out.println(order.getId());
     }
 
-    @Test
+//    @Test
     public void update(){
         CustomOrderDaoImpl dao = new CustomOrderDaoImpl();
         dao.setState(3,true);
